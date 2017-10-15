7f04c5d27e33a2b7afb82bf056369b23ea9ac2a5
Josh Hansen <hansen.joshuaa@gmail.com>
2017-10-14 19:23:48 -0600

    Add Babel ES2015 support and runtime packaging smarts

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,23 @@
+// http://jamesknelson.com/using-es6-in-the-browser-with-babel-6-and-webpack/
+
 module.exports = {
-  entry: './src/app.js',
-  output: {
-    filename: './dist/watrail.js'
-  }
+    entry: './src/app.js',
+    output: {
+        filename: './dist/watrail.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /.js$/,
+                include: /src/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['es2015'],
+                        plugins: ['transform-runtime']
+                    }
+                }
+            }
+        ]
+    }
 }
